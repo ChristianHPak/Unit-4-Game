1a7ad5c888005dbe9951ae45932dd8391851abc0
Christian Pak <iamchristianpak@gmail.com>
2019-06-04 15:27:11 -0600

    added boot strap and changed ids and classes to organize

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,82 @@
-//declare variables
-var userScore = 0;
-var wins = 0;
-var losses = 0;
-var randomScore = 0;
-var buttonScore = 0;
+$(document).ready(function () {
+    //declare variables
+    var userScore = 0;
+    var wins = 0;
+    var losses = 0;
+    var randomScore;
+    var crystal1Num;
+    var crystal2Num;
+    var crystal3Num;
+    var crystal4Num;
 
-// choosing a random score and printing out to html
-function randomNumber() {
-    min = 19;
-    max = 120;
-    randomScore = Math.floor(Math.random() * (max - min + 1)) + min;
-}
-randomNumber();
-$("#randomscore").text(randomScore);
-
-function reset() {
-    randomNumber();
-    $("#randomscore").text(randomScore);
-    $("#userscore").text(userScore);
-    crystalNumber();
-}
-
-function crystalNumber() {
-    buttonScore = Math.floor((Math.random() * 12));
-    $("#emerald").attr(buttonScore);
-    $("#diamond").attr(buttonScore);
-    $("#ruby").attr(buttonScore);
-    $("#topaz").attr(buttonScore);
-    console.log(buttonScore);
-}
-
-crystalNumber();
-
-$(".button").on("click", function () {
-    var crystalValue = ($(this).attr(buttonScore))
-
-    crystalValue = parseInt(crystalValue);
-
-    userScore += crystalValue;
-
-    $("#userscore").text(userScore)
-
-    if(userScore ===randomScore){
-        wins++;
-        reset();
-    }else{
-        losses++;
-        reset();
+    // choosing a random score and printing out to html
+    function Values() {
+        randomScore = Math.floor(Math.random() * 110) + 20;
+        crystal1Num = Math.ceil(Math.random() * 12);
+        crystal2Num = Math.ceil(Math.random() * 12);
+        crystal3Num = Math.ceil(Math.random() * 12);
+        crystal4Num = Math.ceil(Math.random() * 12);
     }
-})
 
-//create function reset()
-//create new number
-//update page
-//create new values to crystals
-//assign each value to a crystal
-//userScore reset back to zero
+    function GameSet() {
+        Values();
+        totalScore = 0;
+        $("#randomscore").text(randomScore);
+        $("#userscore").text(userScore);
+        $("#emerald").attr("data-crystalvalue", crystal1Num);
+        $("#diamond").attr("data-crystalvalue", crystal2Num);
+        $("#ruby").attr("data-crystalvalue", crystal3Num);
+        $("#topaz").attr("data-crystalvalue", crystal4Num);
+        $("#wins").text(wins);
+        $("#losses").text(losses);
+        $("#win-or-lose").text("");
 
-//on click event
-// grab value of clicked crystal    
-// add to player score
+    }
 
-//if statement, if userScore === randomScore
-//player wins
-//wins++
-//reset()
-//else loss
-//losses++;
-// reset()
-//update variables
+    GameSet();
 
-//create new values per crystal
+    function win() {
+        $("#win-or-lose").text("YOU WIN!")
+        wins++;
+        $("#wins").text(wins);
+    }
+
+    function loss() {
+        $("#win-or-lose").text("YOU LOSE!")
+        losses++;
+        $("#losses").text(losses);
+    }
+
+    $(".crystal").hover(function () {
+            $(this).css({
+                opacity: 0.3
+            });
+        },
+        function () {
+            $(this).css({
+                opacity: 1
+            });
+        });
+
+    $(".crystal").on("click", function () {
+        if(userScore >= randomScore){
+            return;
+        }
+        var crystalValue = ($(this).attr("data-crystalvalue"))
+
+        crystalValue = parseInt(crystalValue);
+
+        userScore += crystalValue;
+
+        $("#userscore").text(userScore)
+
+        if (userScore === randomScore) {
+            win();
+            reset();
+
+        } else {
+            loss();
+            reset();
+        }
+    })
+});
